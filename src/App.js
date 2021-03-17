fb95a733c494b36b35f3a57d764b46f303269bdd
bolaji ogidan <bolajiscott@yahoo.com>
2021-03-17 15:51:51 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -3,20 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Navbar">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <li>
+            <a href="#">Browse</a>
+          </li>
+          <li>
+            <a href="#">Activity</a>
+          </li>
+          <li>
+            <a href="#">Ranking</a>
+          </li>
+          <li>
+            <a href="#">BLOG</a>
+          </li>
+          <li>
+            <a href="#">Help</a>
+          </li>
+          <li>
+            <a href="#">Create</a>
+          </li>
+          </ul>
+          <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search items, collections,accounts" title="Type in a category"></input>
       </header>
     </div>
   );
