5142c550d133e996f9fa38aa0b7fe52f607d7ead
bolaji ogidan <bolajiscott@yahoo.com>
2021-03-18 12:45:52 +0100

    clean-up

--- src/App.js
+++ src/App.js
@@ -3,30 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="Navbar">
-      <header className="App-header">
-        <ul>
-          <li>
-            <a href="#">Browse</a>
-          </li>
-          <li>
-            <a href="#">Activity</a>
-          </li>
-          <li>
-            <a href="#">Ranking</a>
-          </li>
-          <li>
-            <a href="#">BLOG</a>
-          </li>
-          <li>
-            <a href="#">Help</a>
-          </li>
-          <li>
-            <a href="#">Create</a>
-          </li>
-          </ul>
-          <input type="text" id="mySearch" onkeyup="myFunction()" placeholder="Search items, collections,accounts" title="Type in a category"></input>
-      </header>
+    <div className="App">
+     
     </div>
   );
 }
